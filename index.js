b4c13248b5946daafc72b314b1c66b5ed81b4994
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-29 14:45:52 -0700

    Updated app boilerplate with additional comments, dev request logging, 404 and 500 error handlers

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+// .env file can hold PORT variable if desired
+require('dotenv').config();
 
-var app = express();
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+
+// Log incoming requests in development:
+if (process.env.RUN_MODE === 'development') {
+  app.use((req, res, next) => {
+    console.log(
+      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`,
+    );
+    next();
+  });
+}
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
 
+// Serve static files from 'public' folder
+// http://expressjs.com/en/starter/static-files.html
+app.use('/public', express.static(__dirname + '/public'));
+
+// Send index.html on requests to root
+// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+// 404 page not found:
+app.get('*', (req, res) => {
+  // Redirect to index
+  res.redirect('/');
+});
 
+// Internal Error Handler:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server error: See Server Logs');
+});
 
-
+// Have server listen on PORT or default to 3000
+// http://localhost:3000/
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
